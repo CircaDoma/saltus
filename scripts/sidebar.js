bfe8dee8b8092c78f54dbdcd280d8e5c38c1bd8d
CircaDoma <kaleb.farar@snapone.com>
2026-04-11 22:32:54 -0600

    about page 98% done

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,25 +1,19 @@
 const navbar = document.getElementById('navbar')
 
-function openSideBar(){
-    navbar.classList.add('show')
-}
-
-function closeSideBar(){
-    navbar.classList.remove('show')
-}
-
-function openMap(){
-    map.classList.add('show')
-}
-
-function closeMap(){
-    map.classList.remove('show')
-}
-
 function openSettings(){
     settings.classList.add('show')
 }
 
 function closeSettings(){
     settings.classList.remove('show')
+}
+
+function openInfo() {
+    info.classList.add('show');
+    document.body.style.overflow = 'hidden';
+}
+
+function closeInfo() {
+    info.classList.remove('show');
+    document.body.style.overflow = '';
 }
